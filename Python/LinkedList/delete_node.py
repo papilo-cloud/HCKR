ced7d761eea4073ed0de391b35bf4c87f9467028
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-11 21:55:05 +0100

    delete a node

---
+++ Python/LinkedList/delete_node.py
@@ -0,0 +1,69 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'deleteNode' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def deleteNode(llist, position):
+    # Write your code here
+    if position == 0:
+        llist = llist.next
+        return llist
+    current_index = 0
+    current_node = llist
+    while current_index < (position - 1):
+        current_node = current_node.next
+        current_index += 1
+    node_index = current_node.next.next
+    current_node.next = node_index
+    return llist
