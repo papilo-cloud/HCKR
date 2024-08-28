03c712069daa8bb0fe01619be5dbad691e7cb1e8
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-28 23:33:17 +0100

    postorder traversal

---
+++ Python/Tree/postorder.py
@@ -0,0 +1,13 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def postOrder(root):
+    #Write your code here
+    if not root:
+        return
+    postOrder(root.left)
+    postOrder(root.right)
+    print(root.info, end=' ')
