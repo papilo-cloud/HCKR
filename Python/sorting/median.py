146bd4d63482b15bf0fcc8af1d8f635ef6a8fd05
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-08-31 16:39:40 +0100

    find the median

---
+++ Python/sorting/median.py
@@ -0,0 +1,11 @@
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    # Write your code here
+    arr.sort()
+    med = len(arr) // 2
+    return arr[med]
